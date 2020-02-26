8eb29e167366d67b2fe5b911415c1345dc25c14f
Robert <bibbycodes@gmail.com>
2020-02-26 14:18:18 +0000

    added dbConn

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
+require('dotenv').config();
 const express = require('express')
 const PORT = process.env.HTTP_PORT || 5000;
 const app = express()
 const cors = require('cors');
 const path = require('path')
+const axios = require('axios')
 
 app.use(cors())
 // app.use(express.static(path.join(__dirname, 'client', 'build')))
 
 app.get('/home', (req, res) => {
   res.json({message : 'Express Template'})
+})
+
+app.get('/cats', (req, res) => {
+  res.json(process.env.API_KEY)
 })
 
 app.listen(PORT, () => {
